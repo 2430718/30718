0208497493ebda5dc033d78780ab41213fcc94f7
2430718 <2430718@sw.hs.kr>
2024-09-23 12:56:16 +0900

    Update looootto.py

--- streamlt_ex-main/looootto.py
+++ streamlt_ex-main/looootto.py
@@ -17,10 +17,14 @@ if st.button('로또를 생성해 주세요!'):
     st.subheader(f'당첨 로또 번호: :blue[{reference_lotto}]')
 
     # 생성할 로또 번호 세트 수 입력
-    num_tickets = st.number_input('생성할 로또 번호 세트 수:', min_value=1, max_value=10, value=1)
+    num_tickets = st.number_input('생성할 로또 번호 세트 수:', min_value=1, max_value=100, value=1)
     
     for i in range(num_tickets):
         ticket = generate_lotto()
         st.subheader(f'{i + 1}. 행운의 번호: :green[{ticket}]')
+        if reference_lotto == ticket:
+            st.subheader(f'행운의 번호: :red[{ticket}]')
+
+
     
-    st.write(f"생성된 시각: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}")
+    
